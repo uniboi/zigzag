a826e338980f28e0e75a2e79e4fb8d31e4ce3bf1
uniboi <kami.0.katze@gmail.com>
2025-08-25 13:41:06 +0200

    update winapi callconv

--- src/kernel32.zig
+++ src/kernel32.zig
@@ -1,5 +1,4 @@
 const std = @import("std");
 const SYSTEM_INFO = std.os.windows.SYSTEM_INFO;
-const WINAPI = std.os.windows.WINAPI;
 
-pub extern "kernel32" fn GetSystemInfo(lpSystemInfo: *SYSTEM_INFO) callconv(WINAPI) void;
+pub extern "kernel32" fn GetSystemInfo(lpSystemInfo: *SYSTEM_INFO) callconv(.winapi) void;
